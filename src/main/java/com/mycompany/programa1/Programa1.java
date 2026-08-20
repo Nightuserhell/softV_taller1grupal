c3a3adfc5c858105ab333218e34bbbdeb94e9a3f
Nigthuserhell <fredyfasebar.1@gmail.com>
2026-08-20 09:14:19 -0500

    programa de prueba Vr1

---
+++ src/main/java/com/mycompany/programa1/Programa1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1;
+
+/**
+ *
+ * @author PC
+ */
+public class Programa1 {
+
+    public static void main(String[] args) {
+        prueba w = new prueba();
+        w.setVisible(true);
+    }
+}
